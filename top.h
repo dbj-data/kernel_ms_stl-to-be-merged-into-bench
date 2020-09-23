2ee72ae946cdce0dad20c2bc376a0d23fac89791
dbjdbj <dbj@dbj.org>
2020-09-23 18:10:40 +0200

    added

---
+++ top.h
@@ -0,0 +1,10 @@
+#pragma once
+/*
+try and have no includes in here ...
+*/
+#define DBJ_APP_NAME "DBJ_KERNEL";
+#define DBJ_APP_VERSION "0.0.1";
+
+#define dbg(x) std::cout << "\n" << std::setw(24) << #x << " : " << (x) 
+
+
